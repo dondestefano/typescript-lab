051b9a2910fa56b55c5747a19a49c829b07ed376
dondestefano <dondestefano@users.noreply.github.com>
2021-01-19 21:38:19 +0100

    Adds basic navigation with typescript.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack'
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+type RootStackParamList = {
+  Article: { itemId: string };
+  List: undefined
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+type ArticleScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Article'
+>;
+
+type ArticleScreenRouteProp = RouteProp<RootStackParamList, 'Article'>;
+
+type Props = {
+  navigation: ArticleScreenNavigationProp;
+  route: ArticleScreenRouteProp
+};
+
+const ListScreen = ({navigation, route}: Props) => {
+  
+  return(
+    <View style={styles.container}>
+      <Text>I'm the list</Text>
+      <Button
+        title="Go to Article"
+        onPress={() => {
+          navigation.navigate('Article', {itemId: 'Text yo!'})
+        }}
+      />
+    </View>
+  )
+}
+
+const ArticleScreen = ({route}: Props) => {
+  const itemId: string = route.params.itemId;
+  return(
+    <View style={styles.container}>
+      <Text>{itemId}</Text>
+    </View>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName={"List"}>
+        <RootStack.Screen name="List" component={ListScreen} />
+        <RootStack.Screen 
+          name="Article" 
+          component={ArticleScreen} 
+          initialParams={{ itemId: "item" }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
