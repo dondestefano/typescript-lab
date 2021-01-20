ac7eda8b62d76188530a9835147ffb14205432cf
dondestefano <dondestefano@users.noreply.github.com>
2021-01-20 22:43:46 +0100

    Adds article view.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { NavigationContainer, RouteProp } from '@react-navigation/native';
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack'
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, FlatList, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 type RootStackParamList = {
-  Article: { itemId: number };
+  Article: { itemId: number, itemTitle: string };
   List: undefined
 };
 
@@ -27,8 +28,25 @@ interface ListItem {
   id: number,
   title: string,
   imageUrl: string,
-  price: number
+  price: number,
   onSale: boolean
+}
+
+interface ArticleScreenItem {
+  id?: number | null,
+  title?: string | null,
+  description?: string | null,
+  category?: string | null,
+  imageUrl: string,
+  price?: number | null,
+  onSale?: boolean | null
+}
+
+const setDefaultArticle: () => ArticleScreenItem = () => {
+    let defaultArticle: ArticleScreenItem = { 
+      imageUrl: "notavailable"};
+
+    return defaultArticle
 }
 
 const ListScreen = ({navigation}: Props) => {
@@ -43,7 +61,6 @@ const ListScreen = ({navigation}: Props) => {
         let isOnSale: boolean = false
 
         if (item.price > 50) {
-          console.log(item.price)
           isOnSale = true
           item.price = Math.round(item.price * 0.8)
         } else { isOnSale = false}
@@ -69,7 +86,7 @@ const ListScreen = ({navigation}: Props) => {
     data={list}
     renderItem={({ item }) =>
     <TouchableOpacity  onPress={() => {
-      navigation.navigate('Article', {itemId: item.id})}}>    
+      navigation.navigate('Article', {itemId: item.id, itemTitle: item.title})}}>    
       <View style={styles.listItemView}>
       <Image source={{uri: item.imageUrl}} style={{width: "25%", height: "100%"}}/>
       <View style={styles.listItemTextView}>
@@ -85,11 +102,56 @@ const ListScreen = ({navigation}: Props) => {
 }
 
 const ArticleScreen = ({route}: Props) => {
-  const itemId: number = route.params.itemId;
+
+  const fetchArticle: () => ArticleScreenItem = () => {
+
+    let url: string = `https://fakestoreapi.com/products/${route.params.itemId}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+
+        let isOnSale: boolean = false
+
+        if (json.price > 50) {
+          isOnSale = true
+          json.price = Math.round(json.price * 0.8)
+        } else { isOnSale = false}
+
+        let result: ArticleScreenItem = { 
+          id: json.id,
+          title: json.title,
+          description: json.description,
+          category: json.category,
+          imageUrl: json.image,
+          price: json.price,
+          onSale: isOnSale };
+
+          setArticle(result)
+
+      });
+    return article
+  }
+
+  const [article, setArticle] = useState<ArticleScreenItem>(() => setDefaultArticle());
+
+  useEffect(() => {
+    if(article.title === undefined) {
+      fetchArticle()
+    }
+  }, [article]);
+
   return(
-    <View style={styles.container}>
-      <Text>{itemId}</Text>
-    </View>
+    <SafeAreaView style={{...styles.container, padding:12}}>
+      <Image source={{uri: article.imageUrl}} style={{width: "60%", height: 300}}/>
+      <View style={styles.listItemTextView}>
+        <Text style={{fontSize: 18, fontWeight: "bold", marginBottom:12}}>{article.title}</Text>
+        <Text style={{fontSize: 18, color: article.onSale? "green" : "black", marginBottom:12 }}>{"$" + article.price}</Text>
+        <Text style={{fontWeight: "bold"}}>Description:</Text>
+        <Text style={{marginBottom:12}}>{article.description}</Text>
+        <Text style={{fontSize: 12, color: "grey" , marginBottom:12}} >{"Category: " + article.category}</Text>
+      </View>
+      <Button title="Add to cart" onPress={() => {console.log("Added to cart")}} />
+    </SafeAreaView>
   )
 }
 
@@ -100,9 +162,10 @@ export default function App() {
       <RootStack.Navigator initialRouteName={"List"}>
         <RootStack.Screen name="List" component={ListScreen} />
         <RootStack.Screen 
-          name="Article" 
+          name="Article"
           component={ArticleScreen} 
-          initialParams={{ itemId: 0 }}
+          initialParams={{ itemId: 0, itemTitle: "Article" }}
+          options={({ route }) => ({ title: route.params.itemTitle })}
         />
       </RootStack.Navigator>
     </NavigationContainer>
